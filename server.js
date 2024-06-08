79b160efd24abe0b0567c20f178de59ab4387f19
Lucio Brito <luciopereirabritoo@gmail.com>
2024-06-08 14:41:23 -0300

    Testing server using fastify package

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-  response.write('oi')
+// const server = createServer((request, response) => {
+//   response.write('oi')
 
-  return response.end();
+//   return response.end();
+// })
+
+// server.listen(2121);
+
+import { fastify } from 'fastify'
+
+const server = fastify();
+
+server.get('/', () => {
+  return 'rota principal';
+})
+server.get('/hello', () => {
+  return 'hello world';
+})
+server.get('/rocketseat', () => {
+  return 'hello rocket';
 })
 
-server.listen(2121);
+server.listen({
+  port: 2121,
+})
